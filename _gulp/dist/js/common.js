0534eead5015cff14c92d2788c462d12359e5972
Dmitriy Khristoev <haossut@gmail.com>
2019-10-16 12:59:22 +0300

    fix

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -73,16 +73,6 @@ $(document).on('ready', function(){
     $(this).prepend('<span class="span">' + ($(this).index() + 1) + '</span>');
   });
 
-  // Chrome Smooth Scroll
-  try {
-    $.browserSelector();
-    if($("html").hasClass("chrome")) {
-      $.smoothScroll();
-    }
-  } catch(err) {
-
-  };
-
   // simpleForm version 2015-09-23 14:30 GMT +2
   simpleForm('form.form-callback');
 });
